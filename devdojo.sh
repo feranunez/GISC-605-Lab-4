52ddbf7500792732f585c76153e3568d1a41a18d
feranunez <149527532+feranunez@users.noreply.github.com>
2023-11-09 03:27:55 +0000

    comments and stuff

--- devdojo.sh
+++ devdojo.sh
@@ -2,8 +2,10 @@
 
 echo "Gello please" $1
 
+# Ask the user for their name
 read -p "What is your name?" name
 
+#Greet the user
 echo "Hi there $name"
 echo "Welcome to GISC605"
 echo "Have a great Wednesday night!"
